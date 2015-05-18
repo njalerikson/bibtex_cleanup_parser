ce5981a380c882bf4cddb6da03f3fd1fa0522f6b
njalerikson <kho1@rice.edu>
2015-05-18 15:01:26 -0500

    more unittests
    added author/editor none check and type customization check

--- bibtexparser/tests/test_customization.py
+++ bibtexparser/tests/test_customization.py
@@ -4,7 +4,7 @@
 from __future__ import unicode_literals
 import unittest
 
-from bibtexparser.customization import getnames, author, editor, convert_to_unicode, homogenize_latex_encoding, page_double_hyphen, keyword
+from bibtexparser.customization import getnames, author, editor, type, convert_to_unicode, homogenize_latex_encoding, page_double_hyphen, keyword
 
 
 class TestBibtexParserMethod(unittest.TestCase):
@@ -50,6 +50,12 @@ class TestBibtexParserMethod(unittest.TestCase):
     ###########
     # author
     ###########
+    def test_author_none(self):
+        record = {'author': None}
+        result = author(record)
+        expected = {}
+        self.assertEqual(result, expected)
+
     def test_author_basic(self):
         record = {'author': 'Foo G. Bar and Lee B. Smith'}
         result = author(record)
@@ -65,6 +71,12 @@ class TestBibtexParserMethod(unittest.TestCase):
     ###########
     # editor
     ###########
+    def test_editor_none(self):
+        record = {'editor': None}
+        result = editor(record)
+        expected = {}
+        self.assertEqual(result, expected)
+
     def test_editor_basic(self):
         record = {'editor': 'Foo G. Bar and Lee B. Smith'}
         result = editor(record)
@@ -78,6 +90,27 @@ class TestBibtexParserMethod(unittest.TestCase):
         expected = {'editor': [ {'ID': 'FooGBar', 'name': ['Foo G.', 'Bar']},
                                 {'ID': 'LeeBSmith', 'name': ['Lee B.', 'Smith']},
                                 {'ID': 'others', 'name': ['', 'others']}]}
+        self.assertEqual(result, expected)
+
+    ###########
+    # type
+    ###########
+    def test_type1(self):
+        record = {'type': 'ARTICLE'}
+        result = type(record)
+        expected = {'type': 'article'}
+        self.assertEqual(result, expected)
+
+    def test_type2(self):
+        record = {'type': 'BoOk'}
+        result = type(record)
+        expected = {'type': 'book'}
+        self.assertEqual(result, expected)
+
+    def test_type3(self):
+        record = {'type': 'journal'}
+        result = type(record)
+        expected = {'type': 'journal'}
         self.assertEqual(result, expected)
 
     ###########
